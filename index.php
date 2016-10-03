ef7aee74aeadf5d92f43804aef7087249b712489
MorisReyes <mario_titi14@hotmail.com>
2016-10-02 20:35:46 -0600

    Aplicación hecha en Ratchet
    Desarrollo de aplicación mobile en Ratchet

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>GameStoreIP</title>
+	<link rel="stylesheet" href="css/ratchet.min.css">
+	<link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+
+<header class="bar bar-nav">
+	<h1 class="title">Cátalogo de Juegos</h1>
+</header>
+
+<div class="bar bar-standard bar-footer">
+	<a class="icon icon-compose" href="#"></a>
+	<a class="icon icon-trash" href="#">
+</div>
+
+<script src="ratchet.min.js"></script>
+
+</body>
+</html>
